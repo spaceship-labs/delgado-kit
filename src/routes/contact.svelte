3a3233fe9aada50ce51de37700bb4fd240da15ab
alex bores <boresrivera@gmail.com>
2021-08-27 18:32:17 -0500

    pagina de contacto

---
+++ src/routes/contact.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import ContactForm from '$lib/ContactForm.svelte';
+	import ContactRibbon from '$lib/ContactRibbon.svelte';
+</script>
+
+<svelte:head>
+	<title>Contacto - Manuel Delgado</title>
+</svelte:head>
+
+
+<main>
+	<ContactForm />
+	
+</main>
+<ContactRibbon />
+
+<style>
+	main{
+		max-width: 1084px;
+		margin: 38px auto;
+	}
+</style>
