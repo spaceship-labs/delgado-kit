49d5ec8fb4dcd5cf9e6b5ff0404d18f82a415877
El Sonny <sonny@spaceshiplabs.com>
2021-08-28 17:06:34 +0200

    prod listing

--- src/lib/ProductListing.svelte
+++ src/lib/ProductListing.svelte
@@ -1,10 +1,12 @@
 <script>
 	import ProductSummary from '$lib/ProductSummary.svelte';
 	export let title;
-	const products = [1,2,3,4,5,6];
+	export let products = [1,2,3,4,5,6];
 </script>
 <div class='container'>
-	<h3 class='strike-header'><span>{title}</span></h3>
+	{#if title}
+		<h3 class='strike-header'><span>{title}</span></h3>
+	{/if}
 	<section>
 		{#each products as product}
 			<ProductSummary {product} />
---
+++ src/routes/products.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import ContactRibbon from '$lib/ContactRibbon.svelte';
+	import BreadCrumbs from '$lib/BreadCrumbs.svelte';
+	import ProductListing from '$lib/ProductListing.svelte';
+	import ProductNav from '$lib/ProductNav.svelte';
+
+	let routes = [
+		{label: 'Inicio', link: '/'},
+		{label: 'Productos', link: '/products'},
+	]
+	let products = [1,2,3,4,5,6,1,2,3];
+</script>
+
+<svelte:head>
+	<title>Productos - Manuel Delgado</title>
+</svelte:head>
+<main>
+	<BreadCrumbs {routes} />
+	<ProductNav />
+	<ProductListing {products}/>
+</main>
+<ContactRibbon />
+
+<style>
+	main{
+		max-width: 1312px;
+		margin: 20px auto;
+	}
+</style>
